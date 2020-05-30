0740c9c01904c44cd7c7a061ce69ec3e9c078c6c
themeur21 <42319336+themeur21@users.noreply.github.com>
2020-05-30 16:39:26 +0200

    c

--- src/containers/Home/RecentPosts/index.js
+++ src/containers/Home/RecentPosts/index.js
@@ -1,38 +1,47 @@
-import React from 'react';
-import './style.css';
-import Card from '../../../components/UI/Card';
+import React from "react";
+import "./style.css";
+import Card from "../../../components/UI/Card";
+import "./style.sass";
 
 /**
-* @author
-* @function RecentPosts
-**/
+ * @author
+ * @function RecentPosts
+ **/
 
-const RecentPosts = (props) => {
-  return(
+const RecentPosts = props => {
+  return (
     <div style={props.style}>
-        <Card style={{marginBottom: '20px'}}>
-            <div className="postImageWrapper">
-                <img src={"https://2.bp.blogspot.com/-ZOY5OAvnKj0/V9_M3_TVTLI/AAAAAAAAD40/UQl4rIoqgi42zaeq0yR_y1gnnchZFxupQCLcB/s1600/3-3.jpg"} alt=""/>
-            </div>
+      <Card style={{ marginBottom: "20px" }}>
+        <div className="postImageWrapper">
+          <img
+            src={
+              "https://2.bp.blogspot.com/-ZOY5OAvnKj0/V9_M3_TVTLI/AAAAAAAAD40/UQl4rIoqgi42zaeq0yR_y1gnnchZFxupQCLcB/s1600/3-3.jpg"
+            }
+            alt=""
+          />
+        </div>
 
-            <div style={{textAlign: 'center'}}>
+        <div style={{ textAlign: "center" }}>
+          <span>Featured</span>
+          <h2>Fitness Mantra To Live Fit Life</h2>
+          <span>posted onJuly 21, 2016 bySora Blogging Tips</span>
+          <p>
+            Midst first it, you're multiply divided. There don't, second his one
+            given the he one third rule fruit, very. Fill. Seed give firm...
+            Extremity direction existence as Dashwood's do up. Securing Marianne
+            led welcomed offended but offering six rapt...
+          </p>
+          <br />
+          <br />
 
-                <span>Featured</span>
-                <h2>Fitness Mantra To Live Fit Life</h2>
-                <span>posted onJuly 21, 2016 bySora Blogging Tips</span>
-                <p>Midst first it, you're multiply divided. There don't, second his one given the he one third rule fruit, very. Fill. Seed give firm... Extremity direction existence as Dashwood's do up. Securing Marianne led welcomed offended but offering six rapt...</p>
+          <button className="try">Read more ➡️</button>
 
-                <button>Read More</button>     
-
-            </div>
-
-            
-
-
-        </Card>
+          <br />
+          <br />
+        </div>
+      </Card>
     </div>
-   )
+  );
+};
 
- }
-
-export default RecentPosts
+export default RecentPosts;
