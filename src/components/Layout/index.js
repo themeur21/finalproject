a5492ca80feda15a901b96cdf6c3cf2fe396346e
themeur21 <42319336+themeur21@users.noreply.github.com>
2020-05-30 17:56:19 +0200

    a

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -15,43 +15,45 @@ const Layout = props => {
         <Sidebar />
       </div>
       <div>
-        <div className="social ">
-          <link
-            href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
-            rel="stylesheet"
-            integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN"
-            crossorigin="anonymous"
-          />
-          <div class="wrapper">
-            <ul>
-              <li class="facebook">
-                <a href="https://www.facebook.com/">
-                  <i class="fa fa-facebook fa-2x" aria-hidden="true" />
-                </a>
-              </li>
-              <li class="twitter">
-                <a href="#">
-                  <i class="fa fa-twitter fa-2x" aria-hidden="true" />
-                </a>
-              </li>
-              <li class="instagram">
-                <a href="#">
-                  <i class="fa fa-instagram fa-2x" aria-hidden="true" />
-                </a>
-              </li>
-              <li class="google">
-                <a href="#">
-                  <i class="fa fa-google fa-2x" aria-hidden="true" />
-                </a>
-              </li>
-              <li class="whatsapp">
-                <a href="#">
-                  <i class="fa fa-whatsapp fa-2x" aria-hidden="true" />
-                </a>
-              </li>
-            </ul>
+        <center>
+          <div className="social ">
+            <link
+              href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+              rel="stylesheet"
+              integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN"
+              crossorigin="anonymous"
+            />
+            <div class="wrapper">
+              <ul>
+                <li class="facebook">
+                  <a href="https://www.facebook.com/">
+                    <i class="fa fa-facebook fa-2x" aria-hidden="true" />
+                  </a>
+                </li>
+                <li class="twitter">
+                  <a href="#">
+                    <i class="fa fa-twitter fa-2x" aria-hidden="true" />
+                  </a>
+                </li>
+                <li class="instagram">
+                  <a href="#">
+                    <i class="fa fa-instagram fa-2x" aria-hidden="true" />
+                  </a>
+                </li>
+                <li class="google">
+                  <a href="#">
+                    <i class="fa fa-google fa-2x" aria-hidden="true" />
+                  </a>
+                </li>
+                <li class="whatsapp">
+                  <a href="#">
+                    <i class="fa fa-whatsapp fa-2x" aria-hidden="true" />
+                  </a>
+                </li>
+              </ul>
+            </div>
           </div>
-        </div>
+        </center>
         <h1>THIS IS FOOTER FOR ALL PAGES</h1>
       </div>
     </React.Fragment>
--- src/containers/Home/RecentPosts/index.js
+++ src/containers/Home/RecentPosts/index.js
@@ -15,7 +15,7 @@ const RecentPosts = props => {
         <div className="postImageWrapper">
           <img
             src={
-              "https://2.bp.blogspot.com/-ZOY5OAvnKj0/V9_M3_TVTLI/AAAAAAAAD40/UQl4rIoqgi42zaeq0yR_y1gnnchZFxupQCLcB/s1600/3-3.jpg"
+              "https://www.dasgerstl.com/fileadmin/_processed_/1/6/csm_das_Gerstl_Zimmer_Fitness__15_von_43__fbac523d46.jpg"
             }
             alt=""
           />
@@ -23,9 +23,10 @@ const RecentPosts = props => {
 
         <div style={{ textAlign: "center" }}>
           <span>Featured</span>
+
           <h2>Fitness Mantra To Live Fit Life</h2>
-          <span>posted onJuly 21, 2016 bySora Blogging Tips</span>
-          <p>
+
+          <p className="textan">
             Midst first it, you're multiply divided. There don't, second his one
             given the he one third rule fruit, very. Fill. Seed give firm...
             Extremity direction existence as Dashwood's do up. Securing Marianne
