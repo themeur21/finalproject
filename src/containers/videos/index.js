9fa587132a2823bb89d55bf2feaa7db43b9833b8
themeur21 <42319336+themeur21@users.noreply.github.com>
2020-05-31 11:28:08 +0200

    .

--- src/containers/videos/index.js
+++ src/containers/videos/index.js
@@ -9,14 +9,26 @@ import './style.scss';
 
 const video = (props) => {
   return(
-    <div>    
-    </div>
-    )
- 
-  }
- 
+<div>
+  <div  >
+<card className="testingp" >
+<iframe width="560" height="315" src="https://www.youtube.com/embed/3bxihqPKF08" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+</card>
+<card className="testingp1" ><iframe width="560" height="315" src="https://www.youtube.com/embed/6-J6mhzcFtE" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></card>
+<card className="testingp2" ><iframe width="560" height="315" src="https://www.youtube.com/embed/XqeCBqY13Uc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></card>
+<card className="testingp3" ><iframe width="560" height="315" src="https://www.youtube.com/embed/nOQpD8aDJeI" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></card>
+<card className="testingp4" ><iframe width="560" height="315" src="https://www.youtube.com/embed/7FCuUhQz5Qw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></card>
+<card className="testingp5" ><iframe width="560" height="315" src="https://www.youtube.com/embed/mdkQYI977Eo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></card>
+</div>
+
+
   
- 
+</div>
+    
+)
+
+}
+  
   export default video;
  
  
